edd12b0da7d2023117dbe171cff2ae99b0c7f329
Nguyen Thanh Luan <luann4099@gmail.com>
2023-03-15 18:21:42 +0700

    update code bt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import {Component} from '@angular/core';
 
+interface Language {
+  name: string;
+  isShow: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +15,27 @@ export class AppComponent {
   //decorator==annotation
   title = 'demo-2';
   public hello: string = "Hello World";
-  public languages:Array<String> = ["Java", "Python", "C#", "C++", "JavaScript"];
+  public languages: Array<string> = ["Java", "Python", "C#", "C++", "JavaScript"];
+  public transformLanguages: Array<Language> = [];
 
-  public showText:boolean = true;
+  constructor() {
+    this.transformLanguages = this.languages.map((lang) => {
+      return {
+        name: lang,
+        isShow: true
+      }
+    })
+    console.log(this.languages);
+    //[{name:"Java", isShow:true}, {name:"Python", isShow:true}, {name:"C#", isShow:true}, {name:"C++", isShow:true}, {name:"JavaScript", isShow:true}}]
+  }
 
-  public addBG:boolean = false;
+  public toggleItem(item: any) {
+    item.isShow = !item.isShow;
+  }
+
+  public showText: boolean = true;
+
+  public addBG: boolean = false;
 
   changeText() {
     this.hello = "Hello Angular";
